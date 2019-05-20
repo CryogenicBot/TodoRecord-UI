a4c1dbfc2df32445f8ce66a9d771377c5181f9a3
verolemieux <veronyque.lemieux@gmail.com>
2019-05-20 14:25:39 -0400

    Using API to modify data

--- src/app/components/dashboard-item/dashboard-item.tsx
+++ src/app/components/dashboard-item/dashboard-item.tsx
@@ -1,32 +1,34 @@
 import * as React from 'react';
 
-import * as styles from './dashboard-item.css';
+// import * as styles from './dashboard-item.css';
 import { Button, Card, Elevation, HTMLTable } from "@blueprintjs/core";
 import { Column, Table, Cell, ICellRenderer } from "@blueprintjs/table";
+import * as _ from 'lodash';
 import API from '../general/api';
 
 interface ItemProps {
-  title: String;
-  data: object[];
+  title: string;
+  data: any[];
 }
 
 interface ItemState {
-
+  data: {
+    thumbnail: string,
+    title: string,
+    day: Date,
+    runTime: number
+  }[];
 }
 
 const moviesResponse =
   [
     {
-      title: "John Wick",
-      itema: "blah",
-      itemb: 24,
-      itemc: "cvxx"
+      "timeSinceRecordCreated": "2018-08-13T00:53:26.877863",
+      "recordApiKey": "245891"
     },
     {
-      title: "Die Hard",
-      itema: "blah",
-      itemb: 24,
-      itemc: "cvxx"
+      "timeSinceRecordCreated": "2019-01-10T00:58:54.677666",
+      "recordApiKey": "562"
     }
   ];
 
@@ -34,21 +36,33 @@ const moviesResponse =
 class DashboardItem extends React.Component<ItemProps, {}> {
   constructor(props: ItemProps) {
     super(props);
+    this.state = {
+      data: []
+    };
   }
 
-  componentWillMount() {
-    // make api call
-    this.setState(() => {
-
+  componentDidMount() {
+    let modifiedData: ItemState = { data: [] };
+    this.props.data.forEach(item => {
+      API.get('https://api.themoviedb.org/3/movie/' + item.recordApiKey, {
+        params: {
+          api_key: '935c8a4dce7a7bc6fcd1ddcc37257332'
+        }
+      }).then((response) => {
+        modifiedData.data.push({
+          thumbnail: 'https://image.tmdb.org/t/p/w92/' + response.data.poster_path,
+          title: response.data.original_title,
+          day: new Date(item.timeSinceRecordCreated),
+          runTime: response.data.runtime
+        });
+        this.setState(() => modifiedData);
+      })
     });
   }
 
-  rowMapper = (row: object) => {
-    row
-  }
-
   render() {
-    const { title, data } = this.props as ItemProps;
+    const { title } = this.props as ItemProps;
+    const { data } = this.state as ItemState;
     if (data.length > 0 && Object.keys(data[0]).length > 0) {
       return <div>
         <h1>{title}</h1>
@@ -56,12 +70,14 @@ class DashboardItem extends React.Component<ItemProps, {}> {
           <HTMLTable interactive={true} bordered={true}>
             <thead>
               <tr>
-                {Object.keys(data[0]).map(header => <th>{header}</th>)}
+                {Object.keys(data[0]).map(header => <th>{_.startCase(header)}</th>)}
               </tr>
             </thead>
             <tbody>
               {data.map(row => <tr>
-                {Object.values(row).map(rowVal => <td>{rowVal}</td>)}
+                {Object.values(row).map(rowVal => <td>
+                  {rowVal.toString().startsWith('http') ? <img src={rowVal.toString()}/> : rowVal.toString()}
+                </td>)}
               </tr>)}
             </tbody>
           </HTMLTable>
